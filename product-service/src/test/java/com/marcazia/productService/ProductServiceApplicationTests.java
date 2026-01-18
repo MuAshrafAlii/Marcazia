4b026bb7f3304b5fd9d6230af7892aa3cf9fc60e
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-18 20:07:12 +0200

    initialize product service with Maven setup and application configuration

---
+++ product-service/src/test/java/com/marcazia/productService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcazia.productService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
