63c6b9221116542011379fe9654e001d0ce33be6
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-19 17:39:56 +0200

    add order and order line entities with payment method enumeration

---
+++ order-service/src/main/java/com/marcazia/ordeService/order/OrderLine.java
@@ -0,0 +1,24 @@
+package com.marcazia.ordeService.order;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+    private double quantity;
+
+}
