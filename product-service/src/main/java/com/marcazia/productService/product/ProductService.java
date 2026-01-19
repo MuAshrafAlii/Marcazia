91348893d838b031795c8861ec5e109d9b4ef23a
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-19 17:04:31 +0200

    add product purchase functionality with response model and exception handling

--- product-service/src/main/java/com/marcazia/productService/product/ProductService.java
+++ product-service/src/main/java/com/marcazia/productService/product/ProductService.java
@@ -1,10 +1,13 @@
 package com.marcazia.productService.product;
 
+import com.marcazia.productService.exception.ProductPurchaseException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -48,5 +51,36 @@ public class ProductService {
             );
         }
 
+        List<ProductPurchaseRequest> storedRequests = request.stream()
+                .sorted(Comparator.comparing(ProductPurchaseRequest::productId))
+                .toList();
+
+        List<ProductPurchaseResponse> purchasedProducts = new ArrayList<>();
+
+        for (int i = 0; i < storedProducts.size(); i++) {
+
+            Product product = storedProducts.get(i);
+            ProductPurchaseRequest productRequest = storedRequests.get(i);
+
+            if (product.getAvailableQuantity() < productRequest.quantity()) {
+                throw new ProductPurchaseException(
+                        "Insufficient stock quantity for product with ID " + productRequest.productId()
+                );
+            }
+
+            double newAvailableQuantity =
+                    product.getAvailableQuantity() - productRequest.quantity();
+
+            product.setAvailableQuantity(newAvailableQuantity);
+            repository.save(product);
+
+            purchasedProducts.add(
+                    mapper.toProductPurchaseResponse(product, productRequest.quantity())
+            );
+        }
+
+        return purchasedProducts;
 
     }
+
+}
