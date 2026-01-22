0a8b63e016695478b225a54ec5e52027aa80fa02
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-22 15:29:01 +0200

    add Payment entity and PaymentMethod enum with JPA auditing support

--- payment-microservice/src/main/java/com/marcazia/payment/MarcaziaPaymentMicroserviceApplication.java
+++ payment-microservice/src/main/java/com/marcazia/payment/MarcaziaPaymentMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.marcazia.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+
 public class MarcaziaPaymentMicroserviceApplication {
 
 	public static void main(String[] args) {
---
+++ payment-microservice/src/main/java/com/marcazia/payment/payment/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.marcazia.payment.payment;
+
+public enum PaymentMethod {
+    PAYPAL,
+    CREDIT_CARD,
+    VISA,
+    MASTERCARD,
+    BITCOIN
+}
