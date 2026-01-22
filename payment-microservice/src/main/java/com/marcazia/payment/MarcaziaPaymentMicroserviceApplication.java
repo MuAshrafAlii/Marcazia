887e4a8b90f0d32cf56b9732a171b283a95bbc52
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-22 15:25:20 +0200

    add initial setup for marcazia payment microservice with Maven configuration

---
+++ payment-microservice/src/main/java/com/marcazia/payment/MarcaziaPaymentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.marcazia.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaziaPaymentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaziaPaymentMicroserviceApplication.class, args);
+	}
+
+}
---
+++ payment-microservice/src/test/java/com/marcazia/payment/MarcaziaPaymentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcazia.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcaziaPaymentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
