309b2b0edc552a431d631525207fcad359157369
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-24 17:04:54 +0200

    add API Gateway module; configure Maven wrapper and project settings

---
+++ api-gateway/src/main/java/com/marcazia/apiGateway/MarcaziaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marcazia.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaziaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaziaApiGatewayApplication.class, args);
+	}
+
+}
