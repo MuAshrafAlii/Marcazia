10bb00e4d108296a98e9b0e3daaa2329cffd87a2
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-24 15:34:57 +0200

    add initial setup for marcazia-notification-service; configure application properties and Maven wrapper

---
+++ notification/src/main/java/com/marcazia/notification/MarcaziaNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcazia.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaziaNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaziaNotificationServiceApplication.class, args);
+	}
+
+}
