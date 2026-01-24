6af693fb72db176e04f7e61569f75cfffc8950ca
Muhammad Ashraf <15Muhammad.Ashraf@gmail.com>
2026-01-24 15:50:39 +0200

    add notification and payment confirmation models; implement Customer, OrderConfirmation, PaymentConfirmation, and PaymentMethod records

---
+++ notification/src/main/java/com/marcazia/notification/kafka/order/OrderConfirmation.java
@@ -0,0 +1,15 @@
+package com.marcazia.notification.kafka.order;
+
+import com.marcazia.notification.kafka.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderConfirmation(
+        String orderReference,
+        BigDecimal totalAmount,
+        PaymentMethod paymentMethod,
+        Customer customer,
+        List<Product> products
+) {
+}
---
+++ notification/src/main/java/com/marcazia/notification/notification/Notification.java
@@ -0,0 +1,29 @@
+package com.marcazia.notification.notification;
+
+import com.marcazia.notification.kafka.order.OrderConfirmation;
+import com.marcazia.notification.kafka.payment.PaymentConfirmation;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    private String id;
+
+    private NotificationType type;
+
+    private LocalDateTime notificationDate;
+
+    private OrderConfirmation orderConfirmation;
+
+    private PaymentConfirmation paymentConfirmation;
+}
